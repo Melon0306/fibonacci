45ee0869586157627964766c52f6b4bfee572a54
Melon0306 <t.kily0121@gmail.com>
2019-02-20 14:12:14 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
+const memo = new Map();//key:順番、value:答えを格納する
+memo.set(0,0);//0,1番目の答えはあらかじめ定義されてるのでこれを格納してるコード（メモ）
+memo.set(1,1);//key1,値1 を登録しておく
 function fib(n){//n番目
-    if(n === 0){
-        return 0;
-    }else if(n === 1){
-        return 1;
+    if(memo.has(n)){//keyを渡す。n番目があるかどうか
+        return memo.get(n);//あるならそのままmemoの値を取得(0と１)
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n,value);//計算したn番目はvalueというのを追加登録してる
+    return value;
 }
 const length = 40;
 for(let i = 0; i<=length; i++){
